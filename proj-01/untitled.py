64b5674f3ec92e1d31994d05a57e613a555a81ad
DJTai <djtai@protonmail.com>
2019-01-28 12:15:24 -0800

    Clean up project code

--- proj-01/untitled.py
+++ proj-01/untitled.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env
 
-# EE 381 Spring 2019 Project 01
-# David Taitingfong
-# 016695521
-# Start  Date: 01-23-2019
-# End Date:    01-??-2019
-# ------------------------------------
-# This project does summary statistics
-# and a Pareto chart
-# ------------------------------------
-
-def main():
+def main(): 
     L = []  # Empty list for data
     v = 1   # Initial value for boolean variable
 
@@ -31,24 +21,8 @@ def main():
 
     print('You inputted the following list of numbers: ', L)
     print('\n')
+    # ---------------------------------------------------------
 
-    N = len(L)  # Number of integers in the list
-    get_mean(L, N)
-    get_median(L, N)
-    get_mode(L)
-
-    # Range - Hi vs lo
-    range = L[len(L)-1] - L[0]
-
-    # Variance
-    # sum((x - mu)^2) / N
-
-    # Standard Deviation
-    # sqrt(variance)
-
-    # Pareto chart created w/ household spending data given
-
-def get_mean(L, N):
     # Calculation of the mean
     s = sum(L)  # The sum of the inputted numbes
     N = len(L)
@@ -56,7 +30,7 @@ def get_mean(L, N):
     print('The mean of the numbers entered is {}'.format(mean))
     # ---------------------------------------------------------
 
-def get_median(L, N):
+    # Calculation of the median
     L.sort()  # Sort the list
     # It is necessary to determine whether or not the length of the list is even
     # or odd
@@ -67,18 +41,6 @@ def get_median(L, N):
         # Odd
         median = L[N//2]
     print('The median of the numbers entered is {}'.format(median))
-
-def get_mode(L):
-    pass
-
-def get_range():
-    pass
-
-def get_variance():
-    pass
-
-def get_std_dev():
-    pass
 
 if __name__ == "__main__":
     main()
